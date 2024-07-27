fbe4e507a1f096d996cf9e4901e4a4df420ad549
DeveloperMusu <165336839+DeveloperMusu@users.noreply.github.com>
2024-07-28 02:04:40 +0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: application/json');
+
+$data = json_decode(file_get_contents('php://input'), true);
+$username = $data['username'];
+$password = $data['password'];
+
+// Dummy users array for example (replace with database logic)
+$users = [
+    ['username' => 'user1', 'password' => 'pass1'],
+    ['username' => 'user2', 'password' => 'pass2']
+];
+
+$success = false;
+foreach ($users as $user) {
+    if ($user['username'] === $username && $user['password'] === $password) {
+        $success = true;
+        break;
+    }
+}
+
+echo json_encode(['success' => $success]);
+?>
